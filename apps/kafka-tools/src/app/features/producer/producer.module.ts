53149ef9ea8215bf4075ae2ba31326ebab700641
Finn <34718806+FinnDore@users.noreply.github.com>
2022-03-14 20:57:52 +0000

    chore(kafka-tools): the themes now use the theme colors to style the angular components

--- apps/kafka-tools/src/app/features/producer/producer.module.ts
+++ apps/kafka-tools/src/app/features/producer/producer.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { ProducerRoutingModule } from './producer-routing.module';
 import { ProducerComponent } from './producer.component';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 @NgModule({
     imports: [
         CommonModule,
 
         // Angular material
         MatButtonModule,
+        MatSlideToggleModule,
 
         // Local
         ProducerRoutingModule,
