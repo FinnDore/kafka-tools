35bcfe4d4d03cf4d9acc306bb91d254a7f2b3f12
Finn <34718806+FinnDore@users.noreply.github.com>
2022-03-16 21:02:38 +0000

    chore(kafka-tools): fixed some linting issues

--- apps/kafka-tools/src/app/app.module.ts
+++ apps/kafka-tools/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './routing/app-routing.module';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { WindowControlsModule } from './modules/window-controls/window-controls.module';
 
 @NgModule({
@@ -15,7 +14,6 @@ import { WindowControlsModule } from './modules/window-controls/window-controls.
 
         // Local modules
         AppRoutingModule,
-        FontAwesomeModule,
         WindowControlsModule,
     ],
     declarations: [AppComponent],
--- apps/kafka-tools/src/app/features/producer/producer.module.ts
+++ apps/kafka-tools/src/app/features/producer/producer.module.ts
@@ -1,12 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { ProducerRoutingModule } from './producer-routing.module';
 import { ProducerComponent } from './producer.component';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
 @NgModule({
     imports: [
         CommonModule,
