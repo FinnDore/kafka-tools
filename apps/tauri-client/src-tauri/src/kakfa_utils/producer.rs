752f76b02f0047c8673e9498fb11da446a26bfba
Finn <34718806+FinnDore@users.noreply.github.com>
2022-03-19 13:58:30 +0000

    chore(tauri-client): moved the kafka producer to its own file

---
+++ apps/tauri-client/src-tauri/src/kakfa_utils/producer.rs
@@ -0,0 +1,54 @@
+use async_trait::async_trait;
+use rdkafka::config::ClientConfig;
+use rdkafka::message::OwnedHeaders;
+use rdkafka::producer::{FutureProducer, FutureRecord};
+use std::time::Duration;
+
+#[async_trait]
+pub trait KafkaProducer {
+    async fn send_kafka_message(&self, topic: String, message: String);
+    fn new(broker: &str) -> Self;
+}
+
+pub struct Producer {
+    producer: FutureProducer,
+}
+
+#[async_trait]
+impl KafkaProducer for Producer {
+    /// Produces the given message to the given topic
+    /// # Arguments
+    ///
+    /// * `topic` - The topic to produce to.
+    /// * `message` - The message to produce.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let my_kafka_producer = Producer::new("localhost:9092");]
+    /// my_kafka_producer.send_kafka_message("test-topic", "My message!")
+    /// ```
+    async fn send_kafka_message(&self, topic: String, message: String) {
+        let delivery_status = &self
+            .producer
+            .send(
+                FutureRecord::to(&topic)
+                    .payload(&message)
+                    .key(&format!("Key"))
+                    .headers(OwnedHeaders::new().add("header_key", "header_value")),
+                Duration::from_secs(0),
+            )
+            .await;
+
+        println!("Future completed. Result: {:?}", delivery_status);
+    }
+
+    fn new(broker: &str) -> Self {
+        let producer: FutureProducer = ClientConfig::new()
+            .set("bootstrap.servers", broker)
+            .set("message.timeout.ms", "5000")
+            .create()
+            .expect("Producer creation error");
+        Self { producer }
+    }
+}
--- apps/tauri-client/src-tauri/src/main.rs
+++ apps/tauri-client/src-tauri/src/main.rs
@@ -2,50 +2,14 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-use async_trait::async_trait;
-use rdkafka::config::ClientConfig;
-use rdkafka::message::OwnedHeaders;
-use rdkafka::producer::{FutureProducer, FutureRecord};
+
 use rdkafka::util::get_rdkafka_version;
-use std::future;
-use std::future::Future;
-use std::io;
-use std::time::Duration;
 use tauri::command;
 use tauri::State;
 
-async fn produce_kafka_message(producer: &FutureProducer, topic_name: String, message: String) {
-    let delivery_status = producer
-        .send(
-            FutureRecord::to(&topic_name)
-                .payload(&message)
-                .key(&format!("Key"))
-                .headers(OwnedHeaders::new().add("header_key", "header_value")),
-            Duration::from_secs(0),
-        )
-        .await;
+mod kakfa_utils;
 
-    println!("Future completed. Result: {:?}", delivery_status);
-}
-
-#[async_trait]
-trait KafkaProducer {
-    async fn send_kafka_message(&self, topic: String, message: String);
-}
-
-struct Producer {
-    producer: FutureProducer,
-}
-
-#[async_trait]
-impl KafkaProducer for Producer {
-    /**
-     * Produces the given message to the given topic
-     */
-    async fn send_kafka_message(&self, topic: String, message: String) {
-        produce_kafka_message(&self.producer, topic, message).await;
-    }
-}
+use crate::kakfa_utils::producer::{KafkaProducer, Producer};
 
 #[command(async)]
 async fn send_kafka_message(
@@ -62,17 +26,9 @@ async fn main() {
     let (version_n, version_s) = get_rdkafka_version();
     print!("rd_kafka_version: 0x{:08x}, {}", version_n, version_s);
 
-    let the_kafka_producer: FutureProducer = ClientConfig::new()
-        .set("bootstrap.servers", "localhost:9092")
-        .set("message.timeout.ms", "5000")
-        .create()
-        .expect("Producer creation error");
-
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![send_kafka_message])
-        .manage(Producer {
-            producer: the_kafka_producer,
-        })
+        .manage(Producer::new("localhost:9092"))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
